66240fc9e44bfa0fd0c4b8c36ed7f53a456e48fd
Michael Royski <mroyski@gmail.com>
2024-05-31 12:13:37 -0400

    extract routes

--- expressserver/app.js
+++ expressserver/app.js
@@ -1,16 +1,15 @@
 require('dotenv').config();
 
-const Pokemon = require('./models/pokemon.js');
+const PokemonRouter = require('./routes/pokemon.js');
 const { connect, seed } = require('./database/config.js');
 const express = require('express');
-const axios = require('axios');
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('build'));
+app.use('/api/pokemon', PokemonRouter);
 
-const POKEMON_API_URL = process.env.POKEMON_API_URL;
 const PORT = process.env.PORT || 8080;
 
 connect()
@@ -23,64 +22,3 @@ connect()
   .catch((error) => {
     console.log(error);
   });
-
-// ROUTES ========================================================
-// Get all captured pokemon
-app.get('/api/pokemon/captured', async (req, res) => {
-  Pokemon.find({})
-    .then((data) => {
-      res.json(data);
-    })
-    .catch((error) => {
-      res.json(error);
-    });
-});
-
-// Get Captured Pokemon details
-app.get('/api/pokemon/captured/:id', async (req, res) => {
-  const { id } = req.params;
-
-  Pokemon.findById(id)
-    .then((pokemon) => {
-      res.json(pokemon);
-    })
-    .catch((error) => {
-      res.json({ error });
-    });
-});
-
-// Catch a Pokemon
-app.post('/api/pokemon/catch', async (req, res) => {
-  try {
-    const pokemon = new Pokemon({
-      pokemonId: req.body.id,
-      name: req.body.name,
-      sprite: req.body.sprite,
-    });
-
-    await pokemon.save();
-    res.json(pokemon);
-  } catch (error) {
-    res.json({ error });
-  }
-});
-
-// Get Pokemon details via external API
-app.get('/api/pokemon/:id', async (req, res) => {
-  const { id } = req.params;
-
-  try {
-    const response = await axios.get(`${POKEMON_API_URL}/pokemon/${id}`);
-    const pokemonData = response.data;
-    const pokemon = {
-      name: pokemonData.name,
-      sprite: pokemonData.sprites.front_default,
-      stats: pokemonData.stats,
-    };
-
-    res.json(pokemon);
-  } catch (error) {
-    console.error('Error fetching Pokemon data:', error);
-    res.status(500).json({ error: 'Failed to fetch Pokemon data' });
-  }
-});
---
+++ expressserver/routes/pokemon.js
@@ -0,0 +1,67 @@
+const axios = require('axios');
+const PokemonRouter = require('express').Router();
+const Pokemon = require('../models/pokemon.js');
+
+const POKEMON_API_URL = process.env.POKEMON_API_URL;
+
+// Get all captured pokemon
+PokemonRouter.get('/captured', async (req, res) => {
+  Pokemon.find({})
+    .then((data) => {
+      res.json(data);
+    })
+    .catch((error) => {
+      res.json(error);
+    });
+});
+
+// Get Captured Pokemon details
+PokemonRouter.get('/captured/:id', async (req, res) => {
+  const { id } = req.params;
+
+  Pokemon.findById(id)
+    .then((pokemon) => {
+      res.json(pokemon);
+    })
+    .catch((error) => {
+      res.json({ error });
+    });
+});
+
+// Catch a Pokemon
+PokemonRouter.post('/catch', async (req, res) => {
+  try {
+    const pokemon = new Pokemon({
+      pokemonId: req.body.id,
+      name: req.body.name,
+      sprite: req.body.sprite,
+    });
+
+    await pokemon.save();
+    res.json(pokemon);
+  } catch (error) {
+    res.json({ error });
+  }
+});
+
+// // Get Pokemon details via external API
+// PokemonRouter.get('/:id', async (req, res) => {
+//   const { id } = req.params;
+
+//   try {
+//     const response = await axios.get(`${POKEMON_API_URL}/pokemon/${id}`);
+//     const pokemonData = response.data;
+//     const pokemon = {
+//       name: pokemonData.name,
+//       sprite: pokemonData.sprites.front_default,
+//       stats: pokemonData.stats,
+//     };
+
+//     res.json(pokemon);
+//   } catch (error) {
+//     console.error('Error fetching Pokemon data:', error);
+//     res.status(500).json({ error: 'Failed to fetch Pokemon data' });
+//   }
+// });
+
+module.exports = PokemonRouter;
