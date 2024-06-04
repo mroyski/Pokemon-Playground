34775a95d6558c927c46b7fd53fcc0fb23653dd2
Michael Royski <mroyski@gmail.com>
2024-06-04 11:21:47 -0400

    update log display format

--- client/src/components/Logs.js
+++ client/src/components/Logs.js
@@ -1,12 +1,36 @@
-const LOG_SIZE = 5;
+import React, { useRef, useEffect } from 'react';
+const LOG_SIZE = 20;
 
 const Logs = ({ logs }) => {
   const recentLogs = logs.slice(0, LOG_SIZE);
+  const containerRef = useRef(null);
+
+  useEffect(() => {
+    containerRef.current.scrollTop = 0;
+  }, [logs]);
+
+  const formatLogDate = (date) => {
+    const currentDate = new Date(date).toLocaleString();
+    return currentDate;
+  };
 
   return (
-    <ul>
+    <ul
+      ref={containerRef}
+      style={{
+        border: '1px solid #ccc',
+        height: '100px',
+        overflowX: 'hidden',
+        overflowY: 'scroll',
+        padding: '5px',
+      }}
+    >
       {recentLogs.map((log) => (
-        <li>{log}</li>
+        <li key={log.timestamp}>
+          {formatLogDate(log.timestamp)}
+          <br></br>
+          {log.data}
+        </li>
       ))}
     </ul>
   );
