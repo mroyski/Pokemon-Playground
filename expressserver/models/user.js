bcf131a4915599d02ff5e63d7dae04fd29347418
Michael Royski <mroyski@gmail.com>
2024-06-05 10:33:10 -0400

    add user model

---
+++ expressserver/models/user.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  tokens: [
+    {
+      token: {
+        type: String,
+        required: true,
+      },
+    },
+  ],
+});
+
+userSchema.methods.verifyPassword = async function (password) {
+  const user = this;
+  const isMatch = await bcrypt.compare(password, user.password);
+  return isMatch;
+};
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
