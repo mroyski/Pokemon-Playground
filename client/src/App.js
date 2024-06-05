14f54af0985c2081d413b2d00c3e2ac8c16531ff
Michael Royski <mroyski@gmail.com>
2024-06-05 13:09:49 -0400

    setup layout

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,11 @@
 import React, { useState } from 'react';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import {
+  createBrowserRouter,
+  Link,
+  Outlet,
+  RouterProvider,
+} from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { Link } from 'react-router-dom';
 import CapturedPokemon from './components/CapturedPokemon';
 import CapturedPokemonDetails from './components/CapturedPokemonDetails';
 import CatchPokemon from './components/CatchPokemon';
@@ -12,22 +16,52 @@ import './App.css';
 
 const queryClient = new QueryClient();
 
+const Navbar = () => {
+  return (
+    <ul>
+      <li>
+        <Link to={'/login/'}>Login</Link>
+      </li>
+      <li>
+        <Link to={'/'}>Catch</Link>
+      </li>
+      <li>
+        <Link to={'/captured/'}>Captured</Link>
+      </li>
+    </ul>
+  );
+};
+
+const AppLayout = () => {
+  return (
+    <>
+      <Navbar />
+      <Outlet />
+    </>
+  );
+};
+
 const router = createBrowserRouter([
   {
-    path: '/',
-    element: <CatchPokemon />,
-  },
-  {
-    path: '/captured',
-    element: <CapturedPokemon />,
-  },
-  {
-    path: '/captured/:id',
-    element: <CapturedPokemonDetails />,
-  },
-  {
-    path: '/login',
-    element: <Login />,
+    element: <AppLayout />,
+    children: [
+      {
+        path: '/',
+        element: <CatchPokemon />,
+      },
+      {
+        path: '/captured',
+        element: <CapturedPokemon />,
+      },
+      {
+        path: '/captured/:id',
+        element: <CapturedPokemonDetails />,
+      },
+      {
+        path: '/login',
+        element: <Login />,
+      },
+    ],
   },
 ]);
 
